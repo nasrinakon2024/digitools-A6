e4912bf8f1bf717e2522ff228687fc43fdffa4c2
nasrinakon2024 <nasrinakon2024@gmail.com>
2026-04-16 21:45:53 +0600

    fix: remove base path for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/digitools-A6/', // এই লাইনটি আপনার রিপোজিটরির নাম অনুযায়ী যোগ করুন
+  // এখানে যদি base লেখা থাকে তবে সেই লাইনটি পুরো মুছে দাও
 })
