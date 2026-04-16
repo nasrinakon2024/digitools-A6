90c7b53569310c48abc4af94a6eb57e7aa144fde
nasrinakon2024 <nasrinakon2024@gmail.com>
2026-04-16 21:11:02 +0600

    fix: update base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/digitools-A6/', // এই লাইনটি আপনার রিপোজিটরির নাম অনুযায়ী যোগ করুন
 })
